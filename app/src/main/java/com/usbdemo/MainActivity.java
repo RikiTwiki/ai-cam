1295c6f7cf7a7ded1a54f12d5f2f73b565439b39
rikitwiki <aibekrenadov@gmail.com>
2023-07-10 12:16:56 +0600

    save chg 10

--- app/src/main/java/com/usbdemo/MainActivity.java
+++ app/src/main/java/com/usbdemo/MainActivity.java
@@ -151,8 +151,18 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
         Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
         checkForUpdates();
 
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+//        View decorView = getWindow().getDecorView();
+//        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 
     //初始化界面控件
@@ -438,7 +448,7 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
 
         ImageView myImageView = (ImageView) findViewById(R.id.myImageView);
         myImageView.setImageResource(R.drawable.gts);  // show the cat picture
-        myImageView.setVisibility(View.GONE);  // make sure the image is visible
+        myImageView.setVisibility(View.VISIBLE);  // make sure the image is visible
 
         if (m_objPreview.StartPreview(m_pHolder)) {
             //预览成功
